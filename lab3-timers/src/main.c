8b9913d4aaac6ca9208da156cb1413d4b452f6f1
dalibor-osu <drevojanekdalibor@gmail.com>
2023-10-11 16:29:22 +0200

    lab 3

---
+++ lab3-timers/src/main.c
@@ -0,0 +1,106 @@
+/***********************************************************************
+ * 
+ * Blink two LEDs using functions from GPIO and Timer libraries. Do not 
+ * use delay library any more.
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2018 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ **********************************************************************/
+
+
+/* Defines -----------------------------------------------------------*/
+#define LED_GREEN PB5  // Arduino Uno on-board LED
+#define LED_RED PB0    // External active-low LED
+
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include <gpio.h>           // GPIO library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Toggle two LEDs using the internal 8- and 16-bit 
+ *           Timer/Counter.
+ * Returns:  none
+ **********************************************************************/
+int main(void)
+{
+    // Set pins where LEDs are connected as output
+    GPIO_mode_output(&DDRB, LED_GREEN);
+    GPIO_mode_output(&DDRB, LED_RED);
+
+    // Configuration of 16-bit Timer/Counter1 for LED blinking
+    // Set the overflow prescaler to 262 ms and enable interrupt
+    TIM1_OVF_262MS
+    TIM1_OVF_ENABLE
+
+    TIM0_OVF_16MS
+    TIM0_OVF_ENABLE
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines, ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+}
+
+
+/* Interrupt service routines ----------------------------------------*/
+/**********************************************************************
+ * Function: Timer/Counter1 overflow interrupt
+ * Purpose:  Toggle on-board LED.
+ **********************************************************************/
+ISR(TIMER1_OVF_vect)
+{
+    PORTB = PORTB ^ (1<<LED_GREEN);
+}
+
+ISR(TIMER0_OVF_vect)
+{
+    static uint8_t no_of_overflows = 0;
+    static uint8_t led_value = 0;
+    static uint8_t blinks = 0;
+
+    no_of_overflows++;
+    if (no_of_overflows >= 6*2)
+    {
+        if (led_value == 0) {
+            GPIO_write_high(&PORTB, LED_RED);
+            blinks++;
+            led_value = 1;
+        }
+        else {
+            GPIO_write_low(&PORTB, LED_RED);
+            led_value = 0;
+        }
+
+        // Do this every 6 x 16 ms = 100 ms
+        no_of_overflows = 0;
+    }
+    // Else do nothing and exit the ISR
+    if (blinks < 5) {
+        TIM0_OVF_16MS
+    } 
+    else if (blinks >= 5 && blinks < 10) {
+        TIM0_OVF_4MS
+    }
+    else if (blinks >= 10)
+    {
+        blinks = 0;
+    }
+}
